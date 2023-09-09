44783ff24e2512adcfd2448a0227c85a3a497972
Ben Peddell <klightspeed@killerwolves.net>
2023-09-10 00:16:55 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,175 @@
+Fridge monitor for Alpicool / Brass Monkey fridges
+
+I created this primarily to log the temperature of my fridge.
+
+## Usage
+positional arguments:
+
+address
+    : Bluetooth address of fridge
+
+options:
+
+-b, --bind
+    : Press settings button on fridge to confirm fridge selection
+
+-l, --loop
+    : Poll at regular intervals (default: query once)
+
+-t POLLINTERVAL, --pollinterval POLLINTERVAL
+    : Poll interval in seconds (default: 10)
+
+## Requirements
+
+This script uses [bleak](https://github.com/hbldh/bleak) as its bluetooth library.
+
+As this script uses bluetooth, you will need a working bluetooth adaptor.
+
+## Technical
+
+This script was put together after extracting the javascript from version 2.0.0 of the Alpicool CAR FRIDGE FREEZER android app (the current 2.2.9 version has the javascript compiled into Hermes bytecode).
+
+The app enables notifications on characteristic 0x1236 of GATT service 0x1234, and sends commands on characteristic 0x1235 of GATT service 0x1234
+
+Note that there is no authentication
+
+* Pairing to the device is not required
+* When pairing no pin is required
+* The "bind" command is a convenience function - i.e. the fridge still accepts commands even if the "bond" command has never been issued or acknowledged
+
+Connecting to the BLE device locks out all other clients and disables BLE advertisements until the client disconnects (though it does seem to show up after a while on the next bluetooth address as a bluetooth headset `JR-bluetooth`)
+
+My fridge has a bluetooth device name of `WT-0001`, and the app also looks for bluetooth device names starting with `A1-`, `AK1-`, `AK2-`, or `AK3-`
+
+### Command and notification structure:
+
+Offset  | Length  | Description
+--------|---------|------------
+0x00    | 2       | Frame header (FE FE)
+0x02    | 1       | Data length
+0x03    | 1       | Command / Response code
+0x04    | len - 3 | Command / Response data
+len - 2 | 2       | Checksum (big-endian sum of all other bytes)
+
+### Command / Response codes:
+
+Code | Name | Description
+-----|------|------------
+0x00 | bind | Bind (Fridge displays APP and sends a bind response after settings button is pressed)
+0x01 | query | Query
+0x02 | setOther | Set
+0x04 | reset | Reset
+0x05 | setLeft | Set Unit 1 (Left) Target
+0x06 | setRight | Set Unit 2 (Right) Target
+
+### Fridge Query Data structure:
+
+All temperature values are signed 8-bit integers represented in the selected temperature unit
+
+Offset | Name | Description
+-------|------|------------
+0x00   | locked | Fridge controls locked (boolean)
+0x01   | poweredOn | Fridge switched on (boolean)
+0x02   | runMode | Fridge running mode (Max, Eco)
+0x03   | batSaver | Low voltage cutout level (Low, Mid, High)
+0x04   | leftTarget | Unit 1 (Left) target temperature
+0x05   | tempMax | Maximum selectable temperature
+0x06   | tempMin | Minimum selectable temperature
+0x07   | leftRetDiff | Unit 1 (Left) hysteresis
+0x08   | startDelay | Start Delay (minutes)
+0x09   | unit | Temperature unit (Celsius, Farenheit)
+0x0A   | leftTCHot | Unit 1 (Left) temperature correction when at or above -6°C
+0x0B   | leftTCMid | Unit 1 (Left) temperature correction when between -12°C and -6°C
+0x0C   | leftTCCold | Unit 1 (Left) temperature correction when below -12°C
+0x0D   | leftTCHalt | Unit 1 (Left) temperature correction when shut down
+0x0E   | leftCurrent | Unit 1 (Left) current temperature
+0x0F   | batPercent | Battery charge level in percent (likely voltage-based) (can be 0x7f if unknown)
+0x10   | batVolInt | Integer portion of battery voltage
+0x11   | batVolDec | Decimal portion of battery voltage
+
+For dual-zone fridges:
+
+Offset | Name | Description
+-------|------|------------
+0x12   | rightTarget | Unit 2 (Right) target temperature
+0x13   | | Not used
+0x14   | | Not used
+0x15   | rightRetDiff | Unit 2 (Right) hysteresis
+0x16   | rightTCHot | Unit 2 (Right) temperature correction when at or above -6°C
+0x17   | rightTCMid | Unit 2 (Right) temperature correction when between -12°C and -6°C
+0x18   | rightTCCold | Unit 2 (Right) temperature correction when below -12°C
+0x19   | rightTCHalt | Unit 2 (Right) temperature correction when shut down
+0x1A   | rightCurrent | Unit 2 (Right) current temperature
+0x1B   | runningStatus | Running Status (Unknown - I don't have a dual-zone fridge to test with)
+
+### Fridge Set Data structure
+
+All temperature values are signed 8-bit integers represented in the selected temperature unit
+
+Offset | Name | Description
+-------|------|------------
+0x00   | locked | Fridge controls locked (boolean)
+0x01   | poweredOn | Fridge switched on (boolean)
+0x02   | runMode | Fridge running mode (Max, Eco)
+0x03   | batSaver | Low voltage cutout level (Low, Mid, High)
+0x04   | leftTarget | Unit 1 (Left) target temperature
+0x05   | tempMax | Maximum selectable temperature
+0x06   | tempMin | Minimum selectable temperature
+0x07   | leftRetDiff | Unit 1 (Left) hysteresis
+0x08   | startDelay | Start Delay (minutes)
+0x09   | unit | Temperature unit (Celsius, Farenheit)
+0x0A   | leftTCHot | Unit 1 (Left) temperature correction when at or above -6°C
+0x0B   | leftTCMid | Unit 1 (Left) temperature correction when between -12°C and -6°C
+0x0C   | leftTCCold | Unit 1 (Left) temperature correction when below -12°C
+0x0D   | leftTCHalt | Unit 1 (Left) temperature correction when shut down
+
+For dual-zone fridges:
+
+Offset | Name | Description
+-------|------|------------
+0x0E   | rightTarget | Unit 2 (Right) target temperature
+0x0F   | | Always zero
+0x10   | | Always zero
+0x11   | rightRetDiff | Unit 2 (Right) hysteresis
+0x12   | rightTCHot | Unit 2 (Right) temperature correction when at or above -6°C
+0x13   | rightTCMid | Unit 2 (Right) temperature correction when between -12°C and -6°C
+0x14   | rightTCCold | Unit 2 (Right) temperature correction when below -12°C
+0x15   | rightTCHalt | Unit 2 (Right) temperature correction when shut down
+0x16   | | Always zero
+0x17   | | Always zero
+0x18   | | Always zero
+
+### App Command flow
+
+When connecting to a fridge, the app sends a bind command and waits for a bind notify message to confirm that you are connecting to the right fridge.
+
+```
+2023-09-05 23:05:12.827173  0x1235  Write   fe fe 03 00 01 ff
+2023-09-05 23:05:14.276572  0x1236  Notify  fe fe 04 00 01 02 01
+```
+
+After the fridge selection has been confirmed, the app sends a query command every 2 seconds.
+
+```
+2023-09-05 23:05:16.358755  0x1235  Write   fe fe 03 01 02 00
+2023-09-05 23:05:16.455384  0x1236  Notify  fe fe 15 01 00 01 00 00 f1 14 ec 02 00 00 00 00 00 00 f3 64 0c 03 05 6c
+2023-09-05 23:05:18.383106  0x1235  Write   fe fe 03 01 02 00
+2023-09-05 23:05:18.466445  0x1236  Notify  fe fe 15 01 00 01 00 00 f1 14 ec 02 00 00 00 00 00 00 f3 64 0c 03 05 6c
+```
+
+Setting the target temperature in the app sends the Set Left/Right Target command with the temperature in the selected unit.
+
+```
+2023-09-05 19:49:20.487666  0x1235  Write   fe fe 03 05 ec 02 f1
+2023-09-05 19:49:20.570163  0x1236  Notify  fe fe 03 05 ec 02 f1
+```
+
+Setting anything else sends a Set command with all of the unchanged values filled in from the previous Query data.
+
+```
+2023-09-05 19:54:14.152600  0x1236  Notify  fe fe 15 01 00 01 00 00 ec 14 ec 02 00 00 00 00 00 00 f7 7f 0b 01 05 83
+2023-09-05 19:54:15.444012  0x1235  Write   fe fe 11 02 00 01 00 02 ec 14 ec 02 00 00 00 00 00 00 04 00
+2023-09-05 19:54:15.530186  0x1236  Notify  fe fe 15 02 00 01 00 02 ec 14 ec 02 00 00 00 00 00 00 f7 7f 0b 01 05 86
+2023-09-05 19:54:15.991368  0x1235  Write   fe fe 03 01 02 00
+2023-09-05 19:54:16.071697  0x1236  Notify  fe fe 15 01 00 01 00 02 ec 14 ec 02 00 00 00 00 00 00 f7 7f 0b 01 05 85
+```
---
+++ fridge.py
@@ -0,0 +1,474 @@
+#!/usr/bin/env python3
+
+import struct
+import asyncio
+import argparse
+import logging
+import sys
+import json
+
+from asyncio import Future
+from enum import Enum
+
+from typing import Optional, Union, Callable, Any
+
+from dataclasses import dataclass
+
+from bleak import BleakClient
+from bleak.exc import BleakError
+from bleak.backends.device import BLEDevice
+from bleak.backends.characteristic import BleakGATTCharacteristic
+
+
+service_uid = '1234'
+service_uuid = '00001234-0000-1000-8000-00805f9b34fb'
+command_uid = '1235'
+command_uuid = '00001235-0000-1000-8000-00805f9b34fb'
+notify_uid = '1236'
+notify_uuid = '00001236-0000-1000-8000-00805f9b34fb'
+
+logger = logging.getLogger(__name__)
+
+
+class FridgeCommand(int, Enum):
+    Bind = 0
+    Query = 1
+    Set = 2
+    Reset = 4
+    SetUnit1Target = 5
+    SetUnit2Target = 6
+
+
+class FridgeRunMode(int, Enum):
+    Max = 0
+    Eco = 1
+
+
+class FridgeBatterySaver(int, Enum):
+    Low = 0
+    Mid = 1
+    High = 2
+
+
+class FridgeTemperatureUnit(int, Enum):
+    Celsius = 0
+    Farenheit = 1
+
+
+@dataclass
+class FridgeUnitData(object):
+    target_temperature: int
+    hysteresis: int
+    temperature_correction_hot: int
+    temperature_correction_mid: int
+    temperature_correction_cold: int
+    temperature_correction_halt: int
+    current_temperature: int
+
+
+@dataclass
+class FridgeData(object):
+    controls_locked: bool
+    powered_on: bool
+    run_mode: FridgeRunMode
+    battery_saver: FridgeBatterySaver
+    max_selectable_temperature: int
+    min_selectable_temperature: int
+    start_delay: int
+    temperature_unit: FridgeTemperatureUnit
+    battery_charge_percent: int
+    battery_voltage: float
+    running_status: Optional[int]
+    unit1: FridgeUnitData
+    unit2: Optional[FridgeUnitData]
+
+
+def decode_unit1_data(data: Union[bytes, bytearray]) -> FridgeUnitData:
+    target_temperature, hysteresis, \
+    temperature_correction_hot, temperature_correction_mid, \
+    temperature_correction_cold, temperature_correction_halt, \
+    current_temperature = \
+        struct.unpack_from('>bxxbxxbbbbb', data, 4)
+
+    return FridgeUnitData(
+        target_temperature = target_temperature,
+        hysteresis = hysteresis,
+        temperature_correction_hot = temperature_correction_hot,
+        temperature_correction_mid = temperature_correction_mid,
+        temperature_correction_cold = temperature_correction_cold,
+        temperature_correction_halt = temperature_correction_halt,
+        current_temperature = current_temperature
+    )
+
+
+def decode_unit2_data(data: Union[bytes, bytearray]) -> Optional[FridgeUnitData]:
+    if len(data) < 28:
+        return None
+
+    target_temperature, hysteresis, \
+    temperature_correction_hot, temperature_correction_mid, \
+    temperature_correction_cold, temperature_correction_halt, \
+    current_temperature = \
+        struct.unpack_from('>bxxbbbbbb', data, 18)
+
+    return FridgeUnitData(
+        target_temperature = target_temperature,
+        hysteresis = hysteresis,
+        temperature_correction_hot = temperature_correction_hot,
+        temperature_correction_mid = temperature_correction_mid,
+        temperature_correction_cold = temperature_correction_cold,
+        temperature_correction_halt = temperature_correction_halt,
+        current_temperature = current_temperature
+    )
+
+
+def decode_fridge_data(data: Union[bytes, bytearray]) -> FridgeData:
+    if len(data) < 18:
+        raise Exception('Packet too short')
+
+    controls_locked, powered_on, run_mode, battery_saver, \
+    max_selectable_temperature, min_selectable_temperature, \
+    start_delay, temperature_unit, \
+    battery_charge_percent, battery_voltage_int, battery_voltage_frac = \
+        struct.unpack_from('>??BBxbbxBBxxxxxBBB', data, 0)
+
+    running_status = None
+
+    if len(data) >= 28:
+        running_status = struct.unpack_from('B', data, 28)
+
+    battery_voltage = battery_voltage_int + battery_voltage_frac / 10
+
+    return FridgeData(
+        controls_locked = controls_locked,
+        powered_on = powered_on,
+        run_mode = FridgeRunMode(run_mode),
+        battery_saver = FridgeBatterySaver(battery_saver),
+        max_selectable_temperature = max_selectable_temperature,
+        min_selectable_temperature = min_selectable_temperature,
+        start_delay = start_delay,
+        temperature_unit = FridgeTemperatureUnit(temperature_unit),
+        battery_charge_percent = battery_charge_percent,
+        battery_voltage = battery_voltage,
+        running_status = running_status,
+        unit1 = decode_unit1_data(data),
+        unit2 = decode_unit2_data(data)
+    )
+
+
+def create_packet(data: bytes) -> bytes:
+    pkt = b'\xFE\xFE' + struct.pack('B', len(data) + 2) + data
+    pkt += struct.pack('>H', sum(int(v) for v in pkt))
+    return pkt
+
+
+def get_packet_data(data: bytes) -> bytes:
+    if len(data) <= 2:
+        raise Exception('Packet is too small')
+
+    if data[:2] != b'\xFE\xFE':
+        raise Exception('Invalid frame header')
+
+    pktlen = struct.unpack_from('B', data, 2)[0]
+
+    if pktlen != len(data) - 3:
+        raise Exception('Content length does not match')
+
+    csum = struct.unpack_from('>H', data[-2:])[0]
+
+    if csum != sum(int(v) for v in data[:-2]):
+        raise Exception('Invalid checksum')
+
+    return data[3:-2]
+
+
+def encode_bind_command() -> bytes:
+    return create_packet(struct.pack('B', FridgeCommand.Bind))
+
+
+def encode_query_command() -> bytes:
+    return create_packet(struct.pack('B', FridgeCommand.Query))
+
+
+def encode_set_command(data: FridgeData) -> bytes:
+    if data.unit2 is None:
+        return create_packet(struct.pack(
+            '>??BBbbbbBBbbbb',
+            FridgeCommand.Set,
+            data.controls_locked, data.powered_on, data.run_mode, data.battery_saver,
+            data.unit1.target_temperature, data.max_selectable_temperature,
+            data.min_selectable_temperature, data.unit1.hysteresis,
+            data.start_delay, data.temperature_unit,
+            data.unit1.temperature_correction_hot, data.unit1.temperature_correction_mid,
+            data.unit1.temperature_correction_cold, data.unit1.temperature_correction_halt
+        ))
+    else:
+        return create_packet(struct.pack(
+            '>??BBbbbbBBbbbbbxxbbbbbxxx',
+            FridgeCommand.Set,
+            data.controls_locked, data.powered_on, data.run_mode, data.battery_saver,
+            data.unit1.target_temperature, data.max_selectable_temperature,
+            data.min_selectable_temperature, data.unit1.hysteresis,
+            data.start_delay, data.temperature_unit,
+            data.unit1.temperature_correction_hot, data.unit1.temperature_correction_mid,
+            data.unit1.temperature_correction_cold, data.unit1.temperature_correction_halt,
+            data.unit2.target_temperature, data.unit2.hysteresis,
+            data.unit2.temperature_correction_hot, data.unit2.temperature_correction_mid,
+            data.unit2.temperature_correction_cold, data.unit2.temperature_correction_halt
+        ))
+
+
+def encode_reset_command() -> bytes:
+    return create_packet(struct.pack('B', FridgeCommand.Reset))
+
+
+def encode_set_unit1_target_command(temp: int) -> bytes:
+    return create_packet(struct.pack('Bb', FridgeCommand.SetUnit1Target, temp))
+
+
+def encode_set_unit2_target_command(temp: int) -> bytes:
+    return create_packet(struct.pack('Bb', FridgeCommand.SetUnit2Target, temp))
+
+
+class Fridge(object):
+    on_query_response: Callable[[FridgeData], Any]
+
+    command_characteristic: Optional[BleakGATTCharacteristic]
+    notify_characteristic: Optional[BleakGATTCharacteristic]
+    client: BleakClient
+
+    _bind_result_future: Optional[Future[int]]
+    _query_result_future: Optional[Future[FridgeData]]
+    _set_result_future: Optional[Future[FridgeData]]
+    _reset_result_future: Optional[Future[FridgeData]]
+    _set_unit1_result_future: Optional[Future[FridgeData]]
+    _set_unit2_result_future: Optional[Future[FridgeData]]
+
+    def __init__(self, client: Union[BleakClient, BLEDevice, str]):
+        if isinstance(client, BleakClient):
+            self.client = client
+        else:
+            self.client = BleakClient(client)
+
+    async def connect(self):
+        for retrynum in range(0, 2):
+            try:
+                await self.client.connect()
+                break
+            except BleakError as e:
+                if e.args[0] == 'failed to discover services, device disconnected':
+                    logger.info('Retrying after connect failed with: %s', e.args[0])
+                    continue
+                raise
+            except TimeoutError:
+                continue
+        else:
+            await self.client.connect()
+
+        for h, c in self.client.services.characteristics.items():
+            if c.service_uuid == service_uid or c.service_uuid == service_uuid:
+                if c.uuid == command_uid or c.uuid == command_uuid:
+                    self.command_characteristic = c
+                elif c.uuid == notify_uid or c.uuid == notify_uuid:
+                    self.notify_characteristic = c
+
+        if self.command_characteristic is None or self.notify_characteristic is None:
+            self.disconnect()
+            raise Exception('Required GATT characteristics not found')
+
+        await self.client.start_notify(self.notify_characteristic, self._notify_callback)
+
+    async def disconnect(self):
+        await self.client.disconnect()
+
+    async def __aenter__(self):
+        await self.connect()
+        return self
+
+    async def __aexit__(self, exc_type, exc_value, traceback):
+        await self.disconnect()
+
+    def _notify_callback(self, sender: BleakGATTCharacteristic, pkt: bytearray):
+        data = get_packet_data(pkt)
+
+        if len(data) < 2:
+            return
+
+        cmd = struct.unpack_from('B', data, 0)[0]
+
+        if cmd == FridgeCommand.Bind:
+            self.notify_bind(struct.unpack_from('B', data, 1)[0])
+        elif cmd == FridgeCommand.Query:
+            self.notify_query(decode_fridge_data(data[1:]))
+        elif cmd == FridgeCommand.Set:
+            self.notify_set(decode_fridge_data(data[1:]))
+        elif cmd == FridgeCommand.Reset:
+            self.notify_reset(decode_fridge_data(data[1:]))
+        elif cmd == FridgeCommand.SetUnit1Target:
+            self.notify_set_unit1_target_temperature(struct.unpack_from('b', data, 1)[0])
+        elif cmd == FridgeCommand.SetUnit2Target:
+            self.notify_set_unit2_target_temperature(struct.unpack_from('b', data, 1)[0])
+
+    def notify_bind(self, data: int):
+        if isinstance(self._bind_result_future, Future):
+            self._bind_result_future.set_result(data)
+
+    def notify_query(self, data: FridgeData):
+        if self.on_query_response is not None:
+            self.on_query_response(data)
+
+        if isinstance(self._query_result_future, Future):
+            self._query_result_future.set_result(data)
+
+    def notify_set(self, data: FridgeData):
+        if isinstance(self._set_result_future, Future):
+            self._set_result_future.set_result(data)
+
+    def notify_reset(self, data: FridgeData):
+        if isinstance(self._reset_result_future, Future):
+            self._reset_result_future.set_result(data)
+
+    def notify_set_unit1_target_temperature(self, data: int):
+        if isinstance(self._set_unit1_result_future, Future):
+            self._set_unit1_result_future.set_result(data)
+
+    def notify_set_unit2_target_temperature(self, data: int):
+        if isinstance(self._set_unit2_result_future, Future):
+            self._set_unit2_result_future.set_result(data)
+
+    async def send_command(self, pkt: bytes):
+        await self.client.write_gatt_char(self.command_characteristic, pkt, response = True)
+
+    async def send_bind_command(self):
+        pkt = encode_bind_command()
+        await self.send_command(pkt)
+
+    async def send_query_command(self):
+        pkt = encode_query_command()
+        await self.send_command(pkt)
+
+    async def send_set_command(self, data: FridgeData):
+        pkt = encode_set_command(data)
+        await self.send_command(pkt)
+
+    async def send_reset_command(self):
+        pkt = encode_reset_command()
+        await self.send_command(pkt)
+
+    async def send_set_unit1_target_temperature_command(self, target_temperature: int):
+        pkt = encode_set_unit1_target_command(target_temperature)
+        await self.send_command(pkt)
+
+    async def send_set_unit2_target_temperature_command(self, target_temperature: int):
+        pkt = encode_set_unit2_target_command(target_temperature)
+        await self.send_command(pkt)
+
+    async def bind(self) -> int:
+        loop = asyncio.get_event_loop()
+        self._bind_result_future = loop.create_future()
+        await self.send_bind_command()
+        return await self._bind_result_future
+
+    async def query(self) -> FridgeData:
+        loop = asyncio.get_event_loop()
+        self._query_result_future = loop.create_future()
+        await self.send_query_command()
+        return await self._query_result_future
+
+    async def set(self, data: FridgeData) -> FridgeData:
+        loop = asyncio.get_event_loop()
+        self._set_result_future = loop.create_future()
+        await self.send_set_command(data)
+        return await self._set_result_future
+
+    async def reset(self) -> FridgeData:
+        loop = asyncio.get_event_loop()
+        self._reset_result_future = loop.create_future()
+        await self.send_reset_command()
+        return await self._reset_result_future
+
+    async def set_unit1_target_temperature(self, target_temperature: int) -> int:
+        loop = asyncio.get_event_loop()
+        self._set_unit1_result_future = loop.create_future()
+        await self.send_set_unit1_target_temperature_command(target_temperature)
+        return await self._set_unit1_result_future
+
+    async def set_unit2_target_temperature(self, target_temperature: int) -> int:
+        loop = asyncio.get_event_loop()
+        self._set_unit2_result_future = loop.create_future()
+        await self.send_set_unit2_target_temperature_command(target_temperature)
+        return await self._set_unit2_result_future
+
+
+def print_fridge_data(data: FridgeData):
+    info = {
+        'on': data.powered_on,
+        'runMode': data.run_mode.name,
+        'lowVoltageLevel': data.battery_saver.name,
+        'batteryVoltage': data.battery_voltage,
+        'batteryChargePercent': data.battery_charge_percent,
+        'temperatureUnit': data.temperature_unit.name,
+        'units': {
+        }
+    }
+
+    if data.unit1 is not None:
+        info['units']['1'] = {
+            'temperature': data.unit1.current_temperature,
+            'target': data.unit1.target_temperature
+        }
+    
+    if data.unit2 is not None:
+        info['units']['2'] = {
+            'temperature': data.unit2.current_temperature,
+            'target': data.unit2.target_temperature
+        }
+
+    print(json.dumps(info))
+
+
+async def run(addr: str, bind: bool, poll: bool, pollinterval: int):
+    async with Fridge(addr) as fridge:
+        if bind:
+            await asyncio.wait_for(fridge.bind(), 30)
+
+        fridge.on_query_response = print_fridge_data
+
+        try:
+            await asyncio.wait_for(fridge.query(), 5)
+        except TimeoutError:
+            pass
+
+        while poll:
+            await asyncio.sleep(pollinterval)
+
+            try:
+                await asyncio.wait_for(fridge.query(), 5)
+            except TimeoutError:
+                pass
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog='fridge.py',
+        description='Fridge monitor for Alpicool / Brass Monkey fridges'
+    )
+
+    parser.add_argument('address', help='Bluetooth address of fridge')
+    parser.add_argument('-b', '--bind', action='store_true', help='Press settings button on fridge to confirm fridge selection')
+    parser.add_argument('-l', '--loop', action='store_true', help='Poll at regular intervals (default: query once)')
+    parser.add_argument('-t', '--pollinterval', type = int, default=10, help='Poll interval in seconds (default: 10)')
+
+    args = parser.parse_args()
+
+    logging.basicConfig()
+
+    try:
+        asyncio.run(run(args.address, args.bind, args.loop, args.pollinterval))
+    except KeyboardInterrupt:
+        sys.stderr.write('Exiting\n')
+        return
+
+if __name__ == '__main__':
+    main()
+
